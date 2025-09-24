4036f5a550cbcca2dbf07a55a2ff5503a5476169
Daria <kurletova@gmail.com>
2025-09-24 12:10:55 +0300

    fix config

--- react-intro/public/config.js
+++ react-intro/public/config.js
@@ -1,6 +1,7 @@
-// public/config.js
-window.CONFIG = {
-  PUBLIC_URL: process.env.NODE_ENV === 'production' 
-    ? 'https://wonderwhy110.github.io/news-reactApp' 
-    : ''
-};
+const isProduction =
+  window.location.hostname !== "localhost" &&
+  window.location.hostname !== "127.0.0.1";
+
+export const PUBLIC_URL = isProduction
+  ? "https://wonderwhy110.github.io/news-reactApp"
+  : "";
