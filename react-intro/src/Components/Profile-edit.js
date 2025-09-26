d61a2b548de8b926dc7ceb49d1c62b12b1cb937d
Daria <kurletova@gmail.com>
2025-09-26 22:50:12 +0300

    apiUrl

--- react-intro/src/Components/Profile-edit.js
+++ react-intro/src/Components/Profile-edit.js
@@ -9,6 +9,7 @@ const getAvatarUrl = (user) => {
 
 function ProfileEdit() {
   const UPLOADS_BASE_URL = process.env.REACT_APP_UPLOADS_BASE_URL;
+  const API_BASE_URL = process.env.REACT_APP_API_URL
   const [user, setUser] = useState({
     name: "",
     avatar: "",
